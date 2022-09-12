1508ff867d02966ce0f83a80e6fbf404b2d96bee
darae lee <61297852+darae07@users.noreply.github.com>
2022-09-12 13:47:05 +0900

    Time: 158 ms (8.32%), Space: 13.8 MB (99.28%) - LeetHub

--- algorithm/leetcode/2-add-two-numbers/2-add-two-numbers.py
+++ algorithm/leetcode/2-add-two-numbers/2-add-two-numbers.py
@@ -5,27 +5,18 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        len1, len2 = self.getNodeLength(l1), self.getNodeLength(l2)
-        head = l1 if len1>=len2 else l2
-        n1 = head
-        n2 = l2 if n1 == l1 else l1
-        has_up = 0
-        while n1:
-            val = n1.val + (n2.val if n2 else 0) 
-            n1.val = val %10 
-            has_up = 1 if val > 9 else 0
-            if has_up:
-                if n1.next:
-                    n1.next.val+=1
-                else:
-                    n1.next = ListNode(1, None)
-            n1 = n1.next
-            if n2:
-                n2 = n2.next
-        return head
-    
+        dummyHead = ListNode(0)
+        curr = dummyHead
+        carry = 0
+        while l1 != None or l2 != None or carry!=0:
+            l1Val = l1.val if l1 else 0
+            l2Val = l2.val if l2 else 0
+            columnSum = l1Val+l2Val + carry
+            carry = columnSum // 10
+            newNode = ListNode(columnSum%10)
+            curr.next = newNode
+            curr = curr.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummyHead.next
         
-    def getNodeLength(self, node: ListNode):
-        if not node:
-            return 0
-        return self.getNodeLength(node.next)+1
